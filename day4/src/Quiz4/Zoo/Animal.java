60229374294201198281ec5bae00f83d8ab66e2c
Ian Patrick Panganiban <ian-p.panganiban@ad.mynt.xyz>
2023-05-09 12:44:49 +0800

    Quiz 4 Submission Zoo

--- day4/src/Quiz4/Zoo/Animal.java
+++ day4/src/Quiz4/Zoo/Animal.java
@@ -4,16 +4,22 @@ public class Animal {
     public String name;
     String type;
     String food;
+    String sound;
+    boolean canChew;
 
     public Visitors displayAnimal() {
         Animal dog = new Animal();
         dog.name = "Coco the Chunky Bully";
         dog.type = "American Bulldog" ;
         dog.food = "Dog Food";
+        dog.sound = "Arf Arf";
+        dog.canChew = true ;
 
         System.out.println("Name: " + dog.name);
         System.out.println("Type: " + dog.type);
         System.out.println("Food: " + dog.food);
+        System.out.println("Sound: " + dog.sound);
+        System.out.println("Can chew food: " + dog.canChew);
         return null;
     }
 
